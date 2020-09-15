c70110663cea63ce246d8c08328f6560d8423137
ArtisAizkraukle <unity.korektors@gmail.com>
2020-09-15 16:26:56 +0300

    How to set image width to be 100% and height to be auto in react native?

--- App.tsx
+++ App.tsx
@@ -2,20 +2,36 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
 
+import myImage from './assets/mia-bg.jpg';
+const { width, height } = Image.resolveAssetSource(myImage);
+
+const win = Dimensions.get('window');
+const ratio = win.width/580; //580 is actual image width
+
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.miaDialogue}>
         <Text style={styles.h1}>Dialogs ar asistentu #MiaVoice</Text>
-        <Image source={require('./img/mia-bg.jpg')} style={styles.ex}/>
+        <Image source={require('./assets/mia-bg.jpg')} style={styles.image}/>
       </View>
-      <Text>1. Pamošanās 07:00 (ieplānots) 07:03 (izdarīts);</Text>
+      <Text hidden>1. Pamošanās 07:00 (ieplānots) 07:03 (izdarīts);</Text>
       <Text>2. Ūdens uzņemšana 07:05 (ieplānots) 07:04 (izdarītsg);</Text>
       <Text>3. Datora ieslēgšana, piebīdīšana 07:10 (ieplānots) 07:11 (izdarīts);</Text>
       <Text>4. Tējas dzeršana 07:15 (ieplānots) Artis: "Mija, es negribu tēju".  ??:?? (nav izdarīts, nav obligāti);</Text>
       <Text>5. Pnnnnozicionēšana no muguras uz labo sānu 07:45 (ieplānots) 07:50 (izdarīts);</Text>
       <Text>6. Brokastis 08:00 (ieplānots) Artis: "Negribu". ??:?? (nav izdarīts, pierakstīts kā atgādinājums 08:30);</Text>
-      <StatusBar style="auto" />
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+      }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 150, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{height: 100, backgroundColor: 'steelblue'}} />
+      </View>
+      <StatusBar hidden/>
     </View>
   );
 }
@@ -23,9 +39,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    //backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    //justifyContent: 'center',
   },
   h1: {
     alignItems: 'center',
@@ -36,7 +52,13 @@ const styles = StyleSheet.create({
     //marginHorizontal: 12,
   },
   ex: {
-width: undefined,//Dimensions.get('window').width,
+//alignItems: 'stretch',//width: undefined,//Dimensions.get('window').width,
 //height: Dimensions.get('window').height
-}
+},
+  image: {
+      flex: 1,
+      alignSelf: 'stretch',
+      width: win.width,
+      height: 210 * ratio, //362 is actual height of image
+  },
 });
